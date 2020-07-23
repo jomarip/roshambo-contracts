a0b6c4e0823024d0e2b2fe6ead526bafe285b510
Jomari Peterson <jomari.peterson@gmail.com>
2020-07-22 22:35:40 -0700

    Updated the Comments for AdminRole
    Providing more clarification on the functionality. Also resolved an issue with account being aacount.

--- box/contracts/Game_merged.sol
+++ box/contracts/Game_merged.sol
@@ -38,7 +38,7 @@ library Roles {
 contract AdminRole {
     using Roles for Roles.Role;
 
-    event AdminAdded(address indexed aaccount);
+    event AdminAdded(address indexed account);
     event AdminRemoved(address indexed account);
 
     Roles.Role private _admins;
@@ -51,24 +51,44 @@ contract AdminRole {
         require(isAdmin(msg.sender));
         _;
     }
-
+    
+     /**
+     * @dev Check if the account role is the admin.
+     * @return bool
+     */
     function isAdmin(address account) public view returns (bool) {
         return _admins.has(account);
     }
 
+     /**
+     * @dev Public/Main function to add specified admin only if initiated by the current admin. Run the _addAdmin function
+     * 
+     */
     function addAdmin(address account) public onlyAdmin {
         _addAdmin(account);
     }
-
+    
+    /**
+     * @dev Remove the message sender as admin.  Run the _removeAdmin function.
+     *
+     */
     function renounceAdmin() public {
         _removeAdmin(msg.sender);
     }
 
+    /**
+     * @dev internal call to add an address to the admin list. Initiated by addAdmin. Emit for logging.
+     * 
+     */
     function _addAdmin(address account) internal {
         _admins.add(account);
         emit AdminAdded(account);
     }
-
+    
+    /**
+     * @dev Internal call to remove an address from the admin list. Initiated by renounceAdmin.  Emit for logging.
+     *
+     */
     function _removeAdmin(address account) internal {
         _admins.remove(account);
         emit AdminRemoved(account);
